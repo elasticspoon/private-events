1efcdcb0c10198dc875f12b010a9ca21b2856298
elasticspoon <quesadillaman@gmail.com>
2022-02-14 16:44:08 -0500

    more basic validations

--- app/models/attended_event.rb
+++ app/models/attended_event.rb
@@ -2,6 +2,6 @@ class AttendedEvent < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  validates :user_id
-  validates :event_id
+  validates :user_id, presence: true
+  validates :event_id, presence: true
 end
