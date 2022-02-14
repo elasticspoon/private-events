e05b0333bcc93bda0f06d26c8ebb5ee6a8df2596
elasticspoon <quesadillaman@gmail.com>
2022-02-14 16:03:48 -0500

    fixed reference

--- db/migrate/20220214021428_create_events.rb
+++ db/migrate/20220214021428_create_events.rb
@@ -3,7 +3,7 @@ class CreateEvents < ActiveRecord::Migration[7.0]
     create_table :events do |t|
       t.datetime :date
       t.string :location
-      t.references :creator, null: false, foreign_key: true
+      t.integer :creator, null: false, foreign_key: true
 
       t.timestamps
     end
