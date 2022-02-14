8ec90d8eb94a79529b9ebea91586f8c09efa987f
elasticspoon <quesadillaman@gmail.com>
2022-02-14 17:07:33 -0500

    added inital devise set-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :attended_events
   has_many :events_created, class_name: 'Event', foreign_key: 'creator_id'
   has_many :events_attended, through: :attended_events, source: :event
