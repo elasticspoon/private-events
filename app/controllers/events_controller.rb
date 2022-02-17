3be338ab5ceb88070c26f67da34a728d0e698ee1
elasticspoon <quesadillaman@gmail.com>
2022-02-16 21:09:29 -0500

    added edit and delete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,8 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, except: %i[index show]
-  before_action :find_event, only: :show
+  before_action :find_event, only: %i[show edit destroy update]
   before_action :build_event, only: %i[new create]
+  before_action :event_owner?, only: %i[edit update destroy]
 
   def index
     @events = Event.includes(:creator, :attendees).all
@@ -11,6 +12,14 @@ class EventsController < ApplicationController
 
   def edit; end
 
+  def update
+    if @event.update(event_params)
+      redirect_to [current_user, @event], notice: 'Event created!'
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
   def new; end
 
   def create
@@ -19,6 +28,11 @@ class EventsController < ApplicationController
     else
       render 'new', status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to root_path, notice: 'Event deleted.'
   end
 
   private
@@ -33,5 +47,12 @@ class EventsController < ApplicationController
 
   def build_event
     @event = current_user.events_created.build
+  end
+
+  def event_owner?
+    unless params[:user_id] == current_user.id.to_s
+      redirect_back fallback_location: root_path,
+                    alert: 'You do not have permission.'
+    end
   end
 end
