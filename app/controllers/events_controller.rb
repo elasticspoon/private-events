54bb09e2729b8eae3f8ad437662b5cfcf8bb43f1
elasticspoon <quesadillaman@gmail.com>
2022-02-16 20:30:11 -0500

    added basic event creation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,8 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
   before_action :find_event, only: :show
+  before_action :build_event, only: %i[new create]
+
   def index
     @events = Event.includes(:creator, :attendees).all
   end
@@ -8,9 +11,27 @@ class EventsController < ApplicationController
 
   def edit; end
 
+  def new; end
+
+  def create
+    if @event.update(event_params)
+      redirect_to [current_user, @event], notice: 'Event created!'
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
   private
 
   def find_event
     @event = Event.includes(:creator, :attendees).find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:date, :location)
+  end
+
+  def build_event
+    @event = current_user.events_created.build
   end
 end
