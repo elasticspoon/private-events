54c76e1ac96d2776d557ed5dd96b5010e6e3372d
elasticspoon <quesadillaman@gmail.com>
2022-05-20 20:18:46 -0400

    fixed making and accepting invite logic

--- app/models/attended_event.rb
+++ app/models/attended_event.rb
@@ -2,42 +2,53 @@ class AttendedEvent < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  attr_accessor :current_user_id
-
   validates :user_id, presence: true
   validates :event_id, presence: true
-  # validates :user, uniqueness: { scope: :event }
-  # validates :accepted?, presence: true, if: :private_event
-  validate :attendance
+  validates :user, uniqueness: { scope: :event, message: 'Invite already exists' }
+  validates :accepted, inclusion: [true, false]
+  # validate :attendance
 
-  def public_invite(invite)
-    errors.add :accepted, 'already exists' if invite
+  def self.accept_or_create_invite(updated_params, current_user_id)
+    return AttendedEvent.new(updated_params).check_invite_perms(current_user_id) unless updated_params[:accepted]
+
+    created_invite = AttendedEvent.find_by(user_id: updated_params[:user_id],
+                                           event_id: updated_params[:event_id]) || AttendedEvent.new(updated_params)
+
+    created_invite.check_attendance_perms(current_user_id)
   end
 
-  def private_invite(invite, event)
-    p invite.inspect
-    p event.inspect
-    p current_user_id
-    p invite&.accepted
-    p user_id
-
-    if invite.nil? && event.creator_id == current_user_id
-      update_attribute(:accepted, false)
-      return
-    end
-    if invite&.accepted? == false && user_id == current_user_id
-      update_attribute(:accepted, true)
-      return
-    end
-
-    errors.add :accepted, 'Not valid'
+  def check_attendance_perms(current_user_id)
+    event.private ? accept_private_invite(current_user_id) : accept_public_invite(current_user_id)
   end
 
-  def attendance
-    invite = AttendedEvent.find_by(event_id: event_id, user_id: user_id)
-    event = Event.find(event_id)
-    p event.inspect
-    return private_invite(invite, event) if event&.private
-    return public_invite(invite) if event&.private == false
+  def accept_public_invite(current_user_id)
+    errors.add :accepted, message: 'Invite already exists.' unless new_record? || accepted == false
+    errors.add :accepted, message: 'You do not have the required permissions.' if current_user_id != user.id
+
+    if errors.empty?
+      self.accepted = true
+      save
+    end
+
+    self
+  end
+
+  def accept_private_invite(current_user_id)
+    errors.add :accepted, message: 'Invite does not exist.' if new_record?
+    errors.add :accepted, message: 'You do not have the required permissions.' if current_user_id != user.id
+
+    if errors.empty?
+      self.accepted = true
+      save
+    end
+
+    self
+  end
+
+  def check_invite_perms(current_user_id)
+    errors.add :accepted, message: 'You do not have the required permissions.' if current_user_id != event.creator_id
+
+    save if errors.empty?
+    self
   end
 end
