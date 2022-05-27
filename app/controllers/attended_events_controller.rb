10d0e0266dd22dbb0153603c4f4a5fca05710bc7
elasticspoon <quesadillaman@gmail.com>
2022-05-27 00:17:21 -0400

    implements logic for extending and accepting

--- app/controllers/attended_events_controller.rb
+++ app/controllers/attended_events_controller.rb
@@ -2,27 +2,39 @@ class AttendedEventsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    event = AttendedEvent.new
-    if event.update(attended_event_params)
-      redirect_to event_path(event.event_id), notice: 'You are now attending the event.'
+    event = AttendedEvent.accept_or_create_invite(attended_event_params, current_user.id)
+
+    if event.errors.empty?
+      redirect_to event_path(event.event_id),
+                  notice: (event.accepted ? 'You are now attending the event.' : 'Invite created.')
     else
       redirect_to event_path(event.event_id), alert: event.errors.full_messages.join(' ')
     end
   end
 
   def destroy
-    event = AttendedEvent.find_by(user_id: current_user.id, event_id: params[:event_id])
-    if event
-      event.destroy
-      redirect_to root_path, notice: 'You are no longer attending the event.'
+    response = AttendedEvent.invite_destroyable?(params[:user_id] || current_user.id, params[:event_id],
+                                                 current_user.id)
+    if response[:result]
+      response[:result].destroy
+      redirect_to root_path, notice: response[:response]
     else
-      redirect_to event_path(event.event_id), alert: 'You are not attending that event.'
+      redirect_to event_path(params[:event_id]), alert: response[:response]
     end
   end
 
   private
 
   def attended_event_params
-    params.require(:attended_event).permit(:event_id, :accepted?).merge({ user_id: current_user.id })
+    fix_params.require(:attended_event).permit(:user_id, :event_id, :accepted)
+  end
+
+  def fix_params
+    values = params[:attended_event]
+    values[:accepted] = values[:accepted] ? true : false
+    values[:user_id] = User.find_by(email: values[:email]).id if values.key?(:email)
+    values[:user_id] = current_user.id unless values[:user_id]
+
+    params
   end
 end
