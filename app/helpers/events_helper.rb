11c35858bac78b235024fdd5656f562cb9faea90
Yuri Bocharov <quesadillaman@gmail.com>
2022-07-31 23:34:52 -0400

    initial pixel perfect front end redesign

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,12 @@ module EventsHelper
   def render_with_display_perms(event)
     render 'event', resource: event if event.viewable_by?(current_user)
   end
+
+  def render_event_rand_size(event)
+    if rand(30) > 5
+      render partial: 'events/event_small', locals: { resource: event }
+    else
+      render partial: 'events/event', locals: { event: }
+    end
+  end
 end
