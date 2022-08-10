ae4d01ef4eed8a5ebb943c6a4c32f17d2d04d331
Yuri Bocharov <quesadillaman@gmail.com>
2022-08-10 15:54:24 -0400

    added layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  layout :layout_by_resource
 
   def not_implemented
     flash.alert = 'Not implemented.'
@@ -10,5 +11,15 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[username name])
+  end
+
+  private
+
+  def layout_by_resource
+    if devise_controller? && action_name == 'new'
+      'login_layout'
+    else
+      'application'
+    end
   end
 end
