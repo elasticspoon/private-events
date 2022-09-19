8c685b96d9bb078ab01b2e8bc5d31b09ff29ff9f
Yuri Bocharov <quesadillaman@gmail.com>
2022-09-19 10:14:10 -0400

    test fix

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -13,7 +13,6 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_length_of(:name).is_at_least(3).is_at_most(30) }
     it { is_expected.to validate_presence_of(:username) }
-    it { is_expected.to validate_uniqueness_of(:username) }
     it { is_expected.to validate_length_of(:username).is_at_least(5) }
   end
 
