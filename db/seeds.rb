facfcd3ffcbcf6439ef2e1e181b3845f220b7dc5
YuriBocharov <quesadillaman@gmail.com>
2023-08-18 17:42:49 -0400

    updates seed script to ensure unique values

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-require 'faker'
+require "faker"
 User.destroy_all
 Event.destroy_all
 UserEventPermission.destroy_all
@@ -14,28 +14,28 @@ VISIBILITY = %w[public public public public public public protected private].fre
 ACCESS = %w[public public public public public public public private].freeze
 PERMISSIONS = %w[attend accept_invite].freeze
 
-30.times do
+50.times do
   User.create!(name: Faker::Name.name,
-               username: Faker::Internet.username(specifier: 5),
-               password: Faker::Internet.password(min_length: 6),
-               email: Faker::Internet.email)
+    username: Faker::Internet.unique.username(specifier: 5),
+    password: Faker::Internet.password(min_length: 6),
+    email: Faker::Internet.unique.email)
 end
 
 User.all.each do |user|
-  5.times do
+  10.times do
     user.events_created
-        .create!(name: Faker::Esport.event,
-                 desc: Faker::Lorem.sentence(word_count: 50, supplemental: true,
-                                             random_words_to_add: 30),
-                 date: Faker::Date.forward(days: 360),
-                 location: Faker::Fantasy::Tolkien.location,
-                 event_privacy: ACCESS.sample,
-                 display_privacy: VISIBILITY.sample)
+      .create!(name: Faker::Esport.event,
+        desc: Faker::Lorem.sentence(word_count: 50, supplemental: true,
+          random_words_to_add: 30),
+        date: Faker::Date.forward(days: 360),
+        location: Faker::Fantasy::Tolkien.location,
+        event_privacy: ACCESS.sample,
+        display_privacy: VISIBILITY.sample)
   end
 end
 
 Event.all.each do |event|
   event.user_event_permissions
-       .create!(user_id: User.all.sample.id,
-                permission_type: PERMISSIONS.sample)
+    .create!(user_id: User.all.sample.id,
+      permission_type: PERMISSIONS.sample)
 end
